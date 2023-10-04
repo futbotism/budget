f8aac3add3f697bc084c9eb29973ef70146d06f7
Andrew <alvino.aj@gmail.com>
2023-10-04 20:28:03 +1100

    updated things

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { ThemeProvider } from '@mui/material'
 import { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import { RecoilRoot } from 'recoil'
+import { RecoilRoot, useRecoilValue } from 'recoil'
 import { useChecked } from 'state/transactoins'
 import { theme } from 'theme'
 import Navigation from './components/navigation'
 import Routes from './components/routes'
 import { useCategories } from 'state/categories'
+import { getIsAuthed } from 'state'
+import Login from 'components/login'
 
 function Auth(props: {
   children?: React.ReactNode
 }) {
-
+  const isAuthed = useRecoilValue(getIsAuthed)
   const { getRows } = useChecked()
   const { getCategories } = useCategories()
-  
+
   useEffect(() => {
     getRows()
     getCategories()
@@ -22,7 +24,11 @@ function Auth(props: {
 
   return (
     <>
-      {props.children}
+      {
+        !isAuthed
+          ? <Login />
+          : props.children
+      }
     </>
   )
 }
@@ -31,14 +37,14 @@ function Auth(props: {
 function App() {
   return (
     <BrowserRouter>
-        <RecoilRoot>
-          <ThemeProvider theme={theme}>
-            <Auth>
-              <Navigation />
-              <Routes />
-            </Auth>
-          </ThemeProvider>
-        </RecoilRoot>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <Navigation />
+          <Auth>
+            <Routes />
+          </Auth>
+        </ThemeProvider>
+      </RecoilRoot>
     </BrowserRouter>
   )
 }
--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,22 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Analysis from './analysis'
-import Login from './login'
 import Upload from './upload'
-import { useRecoilValue } from 'recoil'
-import { getIsAuthed } from 'state'
 
 function Routing() {
-  const isAuthed = useRecoilValue(getIsAuthed)
     return (
         <Routes>
-            {isAuthed
-                ? <>
-                    <Route path="/budget/" element={<Analysis />} />
-                    <Route path="/budget/upload" element={<Upload />} />
-                </>
-                : <>
-                    <Route path="/budget/" element={<Login />} />
-                </>}
+            <Route path="/budget/" element={<Analysis />} />
+            <Route path="/budget/upload" element={<Upload />} />
         </Routes>
     )
 }
